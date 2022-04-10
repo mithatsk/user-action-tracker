4f07ef8558779005968e9cd51658bf5d3ae9f028
diana-dr <dragosdiana22@gmail.com>
2022-04-10 19:17:26 +0200

    Stores log data in a file in json format

--- account-management/src/main/java/com/aprilhorizon/advice/LoggingAdvice.java
+++ account-management/src/main/java/com/aprilhorizon/advice/LoggingAdvice.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import java.util.Arrays;
 
 @Aspect
 @Component
