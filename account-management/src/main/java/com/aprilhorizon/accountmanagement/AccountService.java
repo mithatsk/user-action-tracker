a5b607d40fb8c009276f795b4e767edb14cbe81a
mithatsk <mithatkaskara@gmail.com>
2022-05-15 13:50:01 +0200

    Refactored print statements to be more declarative

--- account-management/src/main/java/com/aprilhorizon/accountmanagement/AccountService.java
+++ account-management/src/main/java/com/aprilhorizon/accountmanagement/AccountService.java
@@ -22,45 +22,50 @@ public class AccountService {
     public Account createAccount(String accountName, String accountNumber) {
         Account account = new Account(accountName, accountNumber);
         accounts.add(account);
-        System.out.println("account created, account number:" + accountNumber);
+        System.out.println("createAccount triggered, for account number: " + accountNumber);
         return account;
     }
 
     public void withdraw(String accountNumber, Double amount) {
-        getAccount(accountNumber).withdraw(amount);
-        System.out.println("withdrawn, account number:" + accountNumber);
+        System.out.println("withdrawn triggered, for account number: " + accountNumber + " withdraw amount: " + amount);
+        getAccountFor(accountNumber).withdraw(amount);
     }
 
     public void deposit(String accountNumber, Double amount) {
-        getAccount(accountNumber).deposit(amount);
-        System.out.println("deposited, account number:" + accountNumber);
+        getAccountFor(accountNumber).deposit(amount);
+        System.out.println("deposit triggered, for account number: " + accountNumber + ", deposited amount: " + amount);
     }
 
     public double getBalance(String accountNumber) {
-        double balance = getAccount(accountNumber).getBalance();
-        System.out.println("balance, account number:" + accountNumber + "balance" + balance);
+        double balance = getAccountFor(accountNumber).getBalance();
+        System.out.println("getBalance triggered, for account number: " + accountNumber + ", balance: " + balance);
         return balance;
     }
 
     public String getAccountName(String accountNumber) {
-        String accountName = getAccount(accountNumber).getAccountName();
-        System.out.println("accountNumber" + accountNumber + "accountName:" + accountNumber);
+        String accountName = getAccountFor(accountNumber).getAccountName();
+        System.out.println("getAccountName triggered, for accountNumber: " + accountNumber + ", accountName:" + accountNumber);
         return accountName;
     }
 
     public void changeName(String accountNumber, String newName) {
-        getAccount(accountNumber).changeName(newName);
-        System.out.println("name changed to" + newName);
+        getAccountFor(accountNumber).changeName(newName);
+        System.out.println("changeName triggered for accountNumber: " + accountNumber + ", newName: " + newName);
     }
 
     public Account getAccount(String accountNumber) {
+        Account account = getAccountFor(accountNumber);
+        System.out.println("getAccount triggered, returned account: " + ASEUtil.asJsonString(account));
+        return account;
+    }
+
+    private Account getAccountFor(String accountNumber) {
         Account account = accounts.stream().filter(a -> a.getAccountNumber().equals(accountNumber)).findFirst().get();
-        System.out.println("account:" + account);
         return account;
     }
 
     public List<Account> getAccounts() {
-        System.out.println("accounts:" + accounts);
+        System.out.println("getAccounts triggered, accounts: " + ASEUtil.asJsonString(accounts));
         return accounts;
     }
 }
